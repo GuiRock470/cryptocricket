0a28160eaf6a402aa654489fff75e9b322c546b5
Guilherme Roque de Souza <groquedesouza@gmail.com>
2024-12-10 12:15:32 -0300

    redirects

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import * as path from 'path';
+import { copyFileSync } from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/cryptocricket',
+  plugins: [
+    react(),
+    {
+      name: 'copy-404',
+      writeBundle() {
+        // Copia o index.html para 404.html
+        copyFileSync('dist/index.html', 'dist/404.html');
+      },
+    },
+  ],
+  base: '/cryptocricket/',
   resolve: {
     alias: {
       'src': path.resolve(__dirname, 'src/'),
